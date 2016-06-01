c7a2d05d6a73fe2eb366446aeb6d4742c0f3d6b6
Stephan Bisaha <bisahas1@gmail.com>
2016-06-01 11:31:03 -0400

    adding our new stuff

---
+++ 02/intro.py
@@ -0,0 +1,19 @@
+print ("Hello world!")
+print ('Hello world!')
+#can add notes
+print (10)
+
+print(10 + 10)
+
+print("hello" + "world!")
+
+
+print ("Hello" + str(10))
+name = "Mr. Soma"
+print ("Hello, " + name)
+
+name = input("What's your name?")
+year_of_birth = input("What year were you born?")
+age = 2016 - int(year_of_birth)
+
+print ("Hello, " + name)
